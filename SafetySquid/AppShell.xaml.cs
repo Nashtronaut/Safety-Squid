a08f20ff4b8e8054ab9f13bf69c2729028bc3a83
Nash Boisvert <fenderfan@live.ca>
2022-06-09 15:37:19 -0600

    Maui rework

---
+++ SafetySquid/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SafetySquid;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SafetySquid/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SafetySquid;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
